c352d9e7717e9867fd7691ba581a48da538f03ae
Karan Singh <kss4382@gmail.com>
2020-03-28 20:30:34 -0400

    fleshing out top news command

--- commands/top.js
+++ commands/top.js
@@ -1,4 +1,5 @@
 const fetch = require('node-fetch')
+const Discord = require('discord.js')
 const NEWS_TK = process.env.NEWS_TK
 
 /*
@@ -13,21 +14,49 @@ module.exports = {
     name: 'top',
     description: 'Get Top News',
     execute(message, args) {
+        
 
         console.log(`The first argument received by top command is ${args[0]}`)
-        let url = `https://newsapi.org/v2/top-headlines?&apiKey=${NEWS_TK}`
+        let url = `https://newsapi.org/v2/top-headlines?apiKey=${NEWS_TK}`
+        let argLength = args.length
 
-        // url = country ? url += `&country=${country}` : url
-        // url = category ? url += `&category=${category}` : url
-        // url = sources ? url += `&sources=${sources}` : url
-        // url = keyword ? url += `&q=${keyword}` : url
-        // console.log(`The modified url is ${url}`)
+        let country = args[0]
+        let category = args[1]
+        let sources = args[2]
+        let q = args[3]
+
+        url = country ? url += `&country=${country}` : url
+        url = category ? url += `&category=${category}` : url
+        url = sources ? url += `&sources=${sources}` : url
+        url = q ? url += `&q=${keyword}` : url
+        url += `&pageSize=5`
+        console.log(`The modified url is ${url}`) 
 
         if(args) {
             fetch(url)
                 .then(res => res.json())
                 .then(json => {
-                    console.log(json)
+                    for(article of json.articles) {
+                        // let source = article.source
+                        // let author = article.author
+                        // let title = article.title
+                        // let description = article.description
+                        // let url = article.url
+                        // let image = article.image
+
+                        let { source, author, title, description, url, urlToImage, content } = article            //attempting destructuring
+
+                        let articleEmbed = new Discord.MessageEmbed()
+                            .setColor('#0099ff')
+                            .setTitle(title)
+                            .setURL(url)
+                            .setDescription(description)
+                            .setImage(urlToImage);
+                        
+                        console.log(articleEmbed.length)
+                        message.channel.send(articleEmbed)
+                    }
+
                 })
         } else {
             
