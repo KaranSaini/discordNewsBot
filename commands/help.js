c41b56756c8ecd7a1214ded299338e751aef0483
Karan Singh <kss4382@gmail.com>
2020-04-01 21:08:46 -0400

    need to improve help comm

--- commands/help.js
+++ commands/help.js
@@ -3,7 +3,10 @@ module.exports = {
     description: 'Information About the Bot',
     execute(message, args) {
         message.channel.send(
-            `The Bot accepts commands in the form of -news [command] [options]`
+            `The Bot accepts commands in the form of -news [command] [options]
+                COMMAND OPTIONS = top, search, help
+                OPTIONS for TOP = country, category, sources, search query
+            `
         )
     }
 }
